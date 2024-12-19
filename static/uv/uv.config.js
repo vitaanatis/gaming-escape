87d7b3a9bb84581cb913d4a64e0a86c15382a713
Grayson <136826478+Gr4ys0n@users.noreply.github.com>
2024-12-19 18:47:01 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
         prefix: '/static/uv/service/',
-        bare: 'https://bare.benrogo.net/',
+        bare: 'https://nodiddydogwasatthediddyparty.global.ssl.fastly.net/bare/',
         encodeUrl: Ultraviolet.codec.xor.encode,
         decodeUrl: Ultraviolet.codec.xor.decode,
         handler: '/static/uv/handler.js',
