154957c884768d1b3f7d03c73ec119b670e5f2c6
Grayson <136826478+Gr4ys0n@users.noreply.github.com>
2025-03-25 09:03:54 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "https://yo.what-in.tf/",
+    bare: "https://bare-sigma.yourmom.eu.org",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
