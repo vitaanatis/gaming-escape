62c9d5863c33897bdee6698003923d7ba291e5a1
Grayson <136826478+Gr4ys0n@users.noreply.github.com>
2025-03-29 02:15:08 +0000

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: "https://nauka.politechnika-nova.edu.pl/",
+    bare: "https://bare-sigma.yourmom.eu.org/",
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
